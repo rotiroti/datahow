a363bf14f339d19a21358b34078636b1bc01f4ab
Marco Rotiroti <marco.rotiroti@gmail.com>
2025-02-07 12:42:37 +0100

    feat: add HTTP server to track unique IP addresses from logs

--- datahow_test.go
+++ datahow_test.go
@@ -1,18 +1,39 @@
 package datahow_test
 
 import (
+	"bytes"
+	"context"
+	"errors"
 	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"sync"
 	"testing"
 
 	"github.com/rotiroti/datahow"
 )
 
+func assertStatusCode(tb testing.TB, got, want int) {
+	tb.Helper()
+
+	if got != want {
+		tb.Errorf("status code returned %d; expected %d", got, want)
+	}
+}
+
 func assertBool(tb testing.TB, got, want bool) {
 	tb.Helper()
 
 	if got != want {
-		tb.Errorf("got %v want %v", got, want)
+		tb.Errorf("got %t; expected %t", got, want)
+	}
+}
+
+func assertError(tb testing.TB, got, want error) {
+	tb.Helper()
+
+	if !errors.Is(got, want) {
+		tb.Errorf("error %v; expected %v", got, want)
 	}
 }
 
@@ -28,7 +49,7 @@ func TestExistOrAdd(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			s := datahow.New()
+			s := datahow.NewInMemory()
 			if tt.name == "dup IP" {
 				_ = s.ExistOrAdd(tt.ip)
 			}
@@ -42,7 +63,7 @@ func TestExistOrAdd(t *testing.T) {
 func TestSafeExistsOrAdd(t *testing.T) {
 	var wg sync.WaitGroup
 
-	s := datahow.New()
+	s := datahow.NewInMemory()
 	maxGoroutines := 256
 	wg.Add(maxGoroutines)
 
@@ -56,4 +77,37 @@ func TestSafeExistsOrAdd(t *testing.T) {
 	}
 
 	wg.Wait()
+}
+
+func TestHandleLog(t *testing.T) {
+	tests := []struct {
+		name    string
+		payload string
+		want    int
+	}{
+		{
+			name:    "empty IP address",
+			payload: `{}`,
+			want:    http.StatusUnprocessableEntity,
+		},
+		{
+			name:    "new IP addrees",
+			payload: `{"timestamp": "2020-06-24T15:27:00.123456Z", "ip": "83.150.59.250", "url": "https://datahow.ch"}`,
+			want:    http.StatusOK,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			store := datahow.NewInMemory()
+			srv := datahow.NewLogServer(store)
+			ctx := context.Background()
+			req, err := http.NewRequestWithContext(ctx, http.MethodPost, "/logs", bytes.NewBufferString(tt.payload))
+			assertError(t, err, nil)
+
+			res := httptest.NewRecorder()
+			srv.ServeHTTP(res, req)
+			assertStatusCode(t, res.Code, tt.want)
+		})
+	}
 }
