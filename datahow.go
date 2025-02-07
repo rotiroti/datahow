a563e8e3a5b3303ad1977fca8c23723399cb43c9
Marco Rotiroti <marco.rotiroti@gmail.com>
2025-02-07 12:42:37 +0100

    feat: add HTTP server to track unique IP addresses from logs

--- datahow.go
+++ datahow.go
@@ -1,6 +1,11 @@
 package datahow
 
-import "sync"
+import (
+	"encoding/json"
+	"net/http"
+	"sync"
+	"time"
+)
 
 // A InMemory represents a concurrent-safe set of IP addresses.
 // It uses a map internally to store unique IPs.
@@ -10,7 +15,7 @@ type InMemory struct {
 }
 
 // New initializes a new InMemory set.
-func New() *InMemory {
+func NewInMemory() *InMemory {
 	return &InMemory{u: make(map[string]struct{})}
 }
 
@@ -27,4 +32,57 @@ func (i *InMemory) ExistOrAdd(ip string) bool {
 	i.u[ip] = struct{}{}
 
 	return false
+}
+
+// LogServer represents an HTTP server that handles incoming log records.
+type LogServer struct {
+	mux   *http.ServeMux
+	store *InMemory
+}
+
+// NewLogServer creates a new LogServer with the provided InMemory storage.
+func NewLogServer(im *InMemory) *LogServer {
+	srv := &LogServer{
+		store: im,
+	}
+	srv.routes()
+
+	return srv
+}
+
+func (l *LogServer) routes() {
+	l.mux = http.NewServeMux()
+	l.mux.HandleFunc("POST /logs", l.handleLog())
+}
+
+func (l *LogServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	l.mux.ServeHTTP(w, r)
+}
+
+func (l *LogServer) handleLog() http.HandlerFunc {
+	type record struct {
+		Timestamp time.Time `json:"timestamp"`
+		IPAddress string    `json:"ip"`
+		URL       string    `json:"url"`
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		var jsonRecord record
+
+		if err := json.NewDecoder(r.Body).Decode(&jsonRecord); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+
+			return
+		}
+
+		if jsonRecord.IPAddress == "" {
+			http.Error(w, "handleLog(): empty IP address", http.StatusUnprocessableEntity)
+
+			return
+		}
+
+		_ = l.store.ExistOrAdd(jsonRecord.IPAddress)
+
+		w.WriteHeader(http.StatusOK)
+	}
 }
